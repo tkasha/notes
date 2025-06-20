6eefc1d57abfbfc21ed1c61081f2186a40ffbc42
Ivan Tkachev <tkasha89@gmail.com>
2025-06-20 12:51:50 +0300

    add notes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_06_20_093347) do
+ActiveRecord::Schema[8.0].define(version: 2025_06_20_095037) do
+  create_table "notes", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_notes_on_user_id"
+  end
+
   create_table "sessions", force: :cascade do |t|
     t.integer "user_id", null: false
     t.string "ip_address"
@@ -28,5 +37,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_06_20_093347) do
     t.index ["email_address"], name: "index_users_on_email_address", unique: true
   end
 
+  add_foreign_key "notes", "users"
   add_foreign_key "sessions", "users"
 end
